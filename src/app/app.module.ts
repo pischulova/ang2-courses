35113fdce7a5c1a3a4f8f1fd2f9c146f2df987ef
Alona <Alona_Pishchulova@epam.com>
2017-03-17 15:48:39 +0200

    refactor structure
    Signed-off-by: Alona <Alona_Pishchulova@epam.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,10 +24,6 @@ import { ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
 import { APP_RESOLVER_PROVIDERS } from './app.resolver';
 import { AppState, InternalStateType } from './app.service';
-import { HomeComponent } from './home';
-import { AboutComponent } from './about';
-import { NoContentComponent } from './no-content';
-import { XLargeDirective } from './home/x-large';
 
 import '../styles/styles.scss';
 import '../styles/headings.css';
@@ -50,11 +46,7 @@ type StoreType = {
 @NgModule({
   bootstrap: [ AppComponent ],
   declarations: [
-    AppComponent,
-    AboutComponent,
-    HomeComponent,
-    NoContentComponent,
-    XLargeDirective
+    AppComponent
   ],
   imports: [ // import Angular's modules
     BrowserModule,
---
+++ src/app/components/courses/course-item/CourseItem.ts
@@ -0,0 +1,6 @@
+export interface CourseItem {
+  title: string;
+  description: string;
+  duration: number;
+  date: Date;
+}
